d77df4ecb3b9fe797c775ab5db55354d554065dd
Swaraj Puppalwar <79976106+UltronTheAI@users.noreply.github.com>
2022-03-16 17:59:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const res = require("express/lib/response");
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server, { cors: { origin: "*" } });
+
+// var vm = {};
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/web/index.html');
+});
+
+// app.get('/api', (req, res) => {
+//     res.send(data(req.url));
+// });
+
+app.get('/GetUid', (req, res) => {
+    res.sendFile(__dirname + '/web/uid.html');
+});
+
+
+
+server.listen(process.env.PORT || 5000);
+
+io.on("connection", (socket) => {
+    console.log("User connected... user id = " + socket.id);
+
+    socket.emit('get-uid', socket.id);
+
+    // setInterval(() => {
+        
+    // }, 400);
+    socket.on('read', (data) => {
+        console.log(data);
+        socket.broadcast.emit('read', data);
+    });
+    socket.on('write', (data) => {
+        console.log(data);
+        socket.broadcast.emit('write', data);
+    });
+    socket.on('d', (data) => {
+        console.log(data);
+        socket.broadcast.emit('d', data);
+    });
+
+    socket.on('disconnect', () =>{
+        
+    });
+    function updatevm(data__) {
+        vm[3] = data__;
+    }
+    
+});
