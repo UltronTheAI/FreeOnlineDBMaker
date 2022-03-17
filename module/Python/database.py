f0fc6bb832fe14121acb88dc3995eb6717c0d79e
Swaraj Puppalwar <79976106+UltronTheAI@users.noreply.github.com>
2022-03-17 18:45:02 +0530

    Add files via upload

---
+++ module/Python/database.py
@@ -0,0 +1,63 @@
+import socketio
+
+socket = socketio.Client()
+
+uid_ = ''
+
+# @socket.event
+# def disconnect():
+#     print("I'm disconnected!")
+
+def uid(uid):
+
+    global uid_
+
+    uid_ = uid
+
+    data = {
+        "method": "read",
+        "var": "Data",
+        "text": "Hello",
+        "uid": "UZ-1RheoS1L3DyMpAAAJ"
+    }
+
+    target = data['uid']
+    var = data['var']
+    mode = data['method']
+
+    if mode == 'read':
+
+        socket.emit('read', [target, var, uid])
+    if mode == 'write':
+
+        socket.emit('write', [target, var, uid, data['text']]) 
+    if mode == 'delete':
+
+        socket.emit('delete', [target, var, uid, data['text']]) 
+
+socket.on('get-uid', uid)
+
+def read(data):
+
+    print(data)
+    socket.disconnect()
+    # socket.dissconnect()
+
+def write(data):
+
+    data1 = data[0]
+    data2 = data[1]
+
+    # print (data1, uid_)
+
+    if str(data1).replace(' ', '') == uid_:
+
+        print (data2)
+        socket.disconnect()
+
+socket.on('read', read)
+socket.on('d', write)
+
+# uid()
+
+socket.connect('https://free-online-db-maker.herokuapp.com')
