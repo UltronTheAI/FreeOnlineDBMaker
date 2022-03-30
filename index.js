8e02ed328d42d02aa0a993686e63376cacbf38db
Swaraj Puppalwar <79976106+UltronTheAI@users.noreply.github.com>
2022-03-29 21:11:24 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const res = require("express/lib/response");
 const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server, { cors: { origin: "*" } });
+const ioc = require('socket.io-client');
 
 
 // var vm = {};
