cbe3b7458e018d6addcf001186c7de5c6fff9b63
Swaraj Puppalwar <79976106+UltronTheAI@users.noreply.github.com>
2022-04-12 03:59:46 -0700

    Update index.js

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ function makeid() {
  charactersLength));
    }
    nlog.push(result);
+   console.log(nlog)
    return result;
 }
 // console.log(makeid())
@@ -299,6 +300,21 @@ io.on("connection", (socket) => {
     socket.on('d', (data) => {
         // console.log(data);
         if (data[0] != uk_id) {
+            // if ()
+            // if (nlog.indexOf(data[2]) != -1) {
+                console.log(data)
+                io.to(data[0]).emit('d', data[1]);
+            // }
+        }
+        if (data[0] == uk_id) {
+            uk_id = data[1];
+        }
+        // socket.broadcast.emit('d', data);
+    });
+    socket.on('d10', (data) => {
+        // console.log(data);
+        if (data[0] != uk_id) {
+            // if ()
             if (nlog.indexOf(data[2]) != -1) {
                 console.log(data)
                 io.to(data[0]).emit('d', data[1]);
