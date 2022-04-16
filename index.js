562fdf60d03e48cebc88b28cb46e085aeafd17a8
Swaraj Puppalwar <79976106+UltronTheAI@users.noreply.github.com>
2022-04-15 18:46:25 -0700

    Update index.js

--- index.js
+++ index.js
@@ -347,9 +347,7 @@ io.on("connection", (socket) => {
                 // }
             }
             if (data[0] == uk_id) {
-                if (io.sockets.sockets[data[0]]) {
-                    io.sockets.sockets[data[0].disconnect();
-                }
+                io.to(data[0]).disconnect();
                 uk_id = data[1];
             }
             delete data;
